1930192f51e96e0f7c381b8d6dac34420cc2fd7b
heetsamber <103236108+heetsamber@users.noreply.github.com>
2023-04-08 08:31:34 +0900

    Create Builder.kt

---
+++ CreataionalPatterns/Builder/Builder.kt
@@ -0,0 +1,35 @@
+
+class Computer{
+    var OS:String
+    var RAM:Int
+    var screenSize:Double
+    var externalMouse:Boolean
+    var externalKeyboard:Boolean
+    var battery:String
+
+    constructor(OS: String, RAM: Int, screenSize: Double,
+                externalMouse: Boolean, externalKeyboard: Boolean, battery: String) {
+        this.OS = OS
+        this.RAM = RAM
+        this.screenSize = screenSize
+        this.externalMouse = externalMouse
+        this.externalKeyboard = externalKeyboard
+        this.battery = battery
+    }
+
+    override fun toString(): String {
+        return (" The required configuration is \n" +
+                " OS : $OS " +
+                " RAM : $RAM \n" +
+                " screenSize : $screenSize \n" +
+                " externalMouse : $externalMouse \n" +
+                " externalKeyboard : $externalKeyboard \n" +
+                " battery : $battery \n"
+        )
+    }
+}
+
+fun main(args: Array<String>) {
+    var comp = Computer("Windows", 8, 14.5, true, false, "Inbulit")
+    println(comp)
+}
