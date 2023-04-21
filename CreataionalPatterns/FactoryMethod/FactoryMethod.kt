3aaafb27b9e62a6e2ac89965d7a620b1682899e9
heetsamber <103236108+heetsamber@users.noreply.github.com>
2023-04-21 20:41:27 +0900

    Update FactoryMethod.kt

--- CreataionalPatterns/FactoryMethod/FactoryMethod.kt
+++ CreataionalPatterns/FactoryMethod/FactoryMethod.kt
@@ -1,1 +1,60 @@
 
+interface ICurrency{
+    fun symbol(): String
+    fun code(): String
+}
+
+class Euro : ICurrency{
+    override fun symbol(): String {
+        return "€"
+    }
+
+    override fun code(): String {
+        return "EUR"
+    }
+}
+
+class Dollar : ICurrency{
+    override fun symbol(): String {
+        return "$"
+    }
+
+    override fun code(): String {
+        return "USD"
+    }
+}
+
+class Won : ICurrency{
+    override fun symbol(): String {
+        return "￦"
+    }
+
+    override fun code(): String {
+        return "WON"
+    }
+}
+
+enum class Country{
+    UnitedStates, Spain, Greece, Korea, Japan
+}
+
+fun currency(country: Country) : ICurrency?{
+    when(country){
+        Country.Spain, Country.Greece -> return Euro()
+        Country.UnitedStates -> return Dollar()
+        Country.Korea -> return Won()
+        else -> return null
+    }
+}
+
+
+fun main(args: Array<String>) {
+    val noCurrencyCode = "NULL"
+
+    println(currency(Country.Greece)?.code() ?: noCurrencyCode)
+    println(currency(Country.Spain)?.code() ?: noCurrencyCode)
+    println(currency(Country.UnitedStates)?.code() ?: noCurrencyCode)
+    println(currency(Country.Korea)?.code() ?: noCurrencyCode)
+    // null
+    println(currency(Country.Japan)?.code() ?: noCurrencyCode)
+}
